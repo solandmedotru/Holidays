8d6333599654fdd6a9d6d2f894e188c83bf505dc
Solomin Andrey <solomin.andrey@gmail.com>
2016-09-28 16:03:53 +0300

    Created action to choose country in spinner

--- app/src/main/java/ru/solandme/holidays/MainActivity.java
+++ app/src/main/java/ru/solandme/holidays/MainActivity.java
@@ -1,7 +1,10 @@
 package ru.solandme.holidays;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("settings", Context.MODE_PRIVATE);
+        String text = String.valueOf(sharedPreferences.getInt("country", 0));
+        textView.setText(text);
     }
 }
--- app/src/main/java/ru/solandme/holidays/Splashscreen.java
+++ app/src/main/java/ru/solandme/holidays/Splashscreen.java
@@ -1,30 +1,68 @@
 package ru.solandme.holidays;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import ru.solandme.holidays.adapters.CustomSpinnerAdapter;
 
 public class Splashscreen extends AppCompatActivity {
 
-    private Spinner countries;
     private int[] flags = {R.mipmap.ic_russia, R.mipmap.ic_usa};
+
+    private Context context;
+
+    private int currentCountrySelected;
+
+    private SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
+        context = getApplicationContext();
+
         initViews();
     }
 
     private void initViews() {
+        sharedPreferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+        currentCountrySelected = sharedPreferences.getInt("country", 0);
 
-        countries = (Spinner) findViewById(R.id.countrySpinner);
-        CustomSpinnerAdapter spinnerAdapter = new CustomSpinnerAdapter(getApplicationContext(), flags, getResources().getStringArray(R.array.countries));
-        countries.setAdapter(spinnerAdapter);
+        Spinner countriesSpinner = (Spinner) findViewById(R.id.countrySpinner);
+        CustomSpinnerAdapter spinnerAdapter = new CustomSpinnerAdapter(context, flags, getResources().getStringArray(R.array.countries));
+        countriesSpinner.setAdapter(spinnerAdapter);
+        countriesSpinner.setSelection(currentCountrySelected);
 
+        countriesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currentCountrySelected = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        Button submitBtn = (Button) findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt("country", currentCountrySelected).apply();
+
+                startActivity(new Intent(Splashscreen.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
