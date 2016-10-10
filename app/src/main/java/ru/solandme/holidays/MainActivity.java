fb38b9b420f25fe501062708cdcd62f5da89e60a
Andrey Solomin <solomin.andrey@gmail.com>
2016-10-10 12:38:27 +0300

    create two themes chose action

--- app/src/main/java/ru/solandme/holidays/MainActivity.java
+++ app/src/main/java/ru/solandme/holidays/MainActivity.java
@@ -7,8 +7,11 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import ru.solandme.holidays.adapters.MyFragmentPagerAdapter;
+import ru.solandme.holidays.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.mainToolbar);
@@ -30,5 +34,25 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.main_sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_theme_light:
+                Utils.changeToTheme(Utils.THEME_MATERIAL_LIGHT, this);
+                return true;
+            case R.id.action_theme_dark:
+                Utils.changeToTheme(Utils.THEME_MATERIAL_DARK, this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
